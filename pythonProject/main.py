54481f2c60cabb7b33cc689cd371d47bb9c41ae7
mentrixmax <mentrixmax@gmail.com>
2024-11-06 15:11:56 -0300

    subindo nova rota

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -11,5 +11,16 @@ def hello():
         return "Hello, "+name+" World!, yor age is: "+age
     return "Name and age is required!"
 
+
+@app.route('/soma', methods=['GET'])
+def soma():
+    a = request.args.get("a")
+    b = request.args.get("b")
+    if (a is not None and b is not None):
+        return str(int(a) + int(b))
+    return "paramentros faltando"
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
